13ce948576b1854028add7a18a6f711f0e2d509c
Henrique Ambiel <henriqueambiel@gmail.com>
2025-02-11 21:27:20 -0300

    Aula 1

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,12 @@
+//Exibe título na tela do jogo
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+//Exibe subtítulo do jogo
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+
+function verifyKick()
+{
+    console.log('Teste')
+}
