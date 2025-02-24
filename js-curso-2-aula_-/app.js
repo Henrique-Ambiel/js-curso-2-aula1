8d7f20b5e5c81b7e612f6022c7520cd8a976fe80
Henrique Ambiel <henriqueambiel@gmail.com>
2025-02-24 01:38:01 -0300

    Aula 2

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,12 +1,22 @@
-//Exibe título na tela do jogo
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+let secretNumber = generateSecretNumber();
 
-//Exibe subtítulo do jogo
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10';
+// Função que exibe um texto em uma tag HTML
+function displayTextOnScreen(tag, text){ 
+    let element = document.querySelector(tag)
+    element.innerHTML = text;  
+}
 
+displayTextOnScreen('h1', 'Jogo do Número Secreto');
+displayTextOnScreen('p', 'Escolha um número de 1 a 10');
+
+// Verifca no console se o botão foi clicado
 function verifyKick()
 {
-    console.log('Teste')
+    let kick = document.querySelector('input').value;
+    console.log(kick == secretNumber);
+}
+
+// Funbção que gera o número secreto
+function generateSecretNumber(){
+    return parseInt(Math.random() * 10 + 1);
 }
