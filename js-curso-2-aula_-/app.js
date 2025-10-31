4f94dc7104b76e379e7a86546f5650933a8a0c3a
Henrique-Ambiel <146786119+Henrique-Ambiel@users.noreply.github.com>
2025-10-31 01:11:37 -0300

    Liberando botão de novo jogo

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -7,36 +7,52 @@ function displayTextOnScreen(tag, text){
     element.innerHTML = text;  
 }
 
-displayTextOnScreen('h1', 'Jogo do Número Secreto');
-displayTextOnScreen('p', 'Escolha um número de 1 a 10');
+// Função que imprime o título e a instrução do jogo
+function printSecretNumber(){
+    displayTextOnScreen('h1', 'Jogo do Número Secreto');
+    displayTextOnScreen('p', 'Escolha um número de 1 a 10');
+}
+
+printSecretNumber();
 
 // Verifca no console se o botão foi clicado
 function verifyKick()
 {
     let kick = document.querySelector('input').value;
    
-    if(kick == secretNumber){
+    if(kick == secretNumber){ //Se o número digitado for igual ao número secreto
         displayTextOnScreen('h1', 'VOCÊ ACERTOU!!!');
         attemptWord = attempt > 1 ? 'Tentativas' : 'Tentativa';
         let attemptMenssage = `Você acertou o número com ${attempt} ${attemptWord}`;
         displayTextOnScreen('p', attemptMenssage);
         document.getElementById('reiniciar').removeAttribute('disabled');
         
-    } else if(kick < secretNumber){
+    } else if(kick < secretNumber){ //Se o número digitado for menor que o número secreto
         displayTextOnScreen('p', 'O número secreto é maior');
-    } else {
+        cleanCamp();
+    } else { //Se o número digitado for maior que o número secreto
         displayTextOnScreen('p', 'O número secreto é menor');
+        cleanCamp();
     }
     attempt++;
-    cleanCamp();
 }
 
-// Funbção que gera o número secreto
+// Função que gera o número secreto
 function generateSecretNumber(){
     return parseInt(Math.random() * 10 + 1);
 }
 
+//Função que limpa o campo de input
 function cleanCamp() {
     kick = document.querySelector('input');
     kick.value = '';
+}
+
+// Função que reinicia o jogo
+function restartGame(){
+    attempt = 1;
+    secretNumber = generateSecretNumber();
+    printSecretNumber();
+    cleanCamp();
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
 }
